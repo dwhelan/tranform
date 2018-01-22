b3c36ded30d467c499021b8d2639fda14b3b7eee
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-22 00:46:51 -0500

    Additional test cases.

--- test/transform/map_test.exs
+++ test/transform/map_test.exs
@@ -10,29 +10,33 @@ defmodule Transform.MapTest do
     defstruct [:a, :b, :c]
   end
 
-  describe "supported types" do
+  describe "transform" do
     test "struct -> struct" do
-      assert Map.transform(source(), Target) == target()
+      assert Map.transform(%Source{a: "a", b: 42, c: false}, %Target{}) == %Target{a: "a", b: 42, c: false}
+    end
+
+    test "struct -> struct module" do
+      assert Map.transform(%Source{a: "a", b: 42, c: false}, Target) == %Target{a: "a", b: 42, c: false}
     end
 
     test "struct -> map" do
-      assert Map.transform(source(), %{}) == map()
+      assert Map.transform(%Source{a: "a", b: 42, c: false}, %{}) == %{a: "a", b: 42, c: false}
     end
 
     test "map -> struct" do
-      assert Map.transform(map(), Target) == target()
+      assert Map.transform(%{a: "a", b: 42, c: false}, %Target{}) == %Target{a: "a", b: 42, c: false}
+    end
+
+    test "map -> struct module" do
+      assert Map.transform(%{a: "a", b: 42, c: false}, Target) == %Target{a: "a", b: 42, c: false}
     end
 
     test "map -> map" do
-      assert Map.transform(map(), %{}) == map()
+      assert Map.transform(%{a: "a", b: 42, c: false}, %{}) == %{a: "a", b: 42, c: false}
     end
   end
 
   test "should default to creating a target map" do
-    assert Map.transform(source()) == map()
+    assert Map.transform(%Source{a: "a", b: 42, c: false}) == %{a: "a", b: 42, c: false}
   end
-
-  defp source, do: %Source{a: "a", b: 42, c: false}
-  defp target, do: %Target{a: "a", b: 42, c: false}
-  defp map,    do:       %{a: "a", b: 42, c: false}
 end
