85c7dba54d9eb57bda83992f37f5a18e14c30e91
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-23 20:13:38 -0500

    Refactored map transformer

--- lib/transform/struct.ex
+++ lib/transform/struct.ex
@@ -6,17 +6,9 @@ defmodule Transform.Struct do
   def transform(source, target = %{__struct__: _}) do
     source
     |> Map.keys
-    |> reject_meta_keys
+    |> Transform.Map.reject_meta_keys
     |> copy_values(source, target)
   end
-
-  defp reject_meta_keys keys do
-    Enum.reject keys, &meta_key?(&1)
-  end
-
-  defp meta_key?(key) when is_atom(key),   do: meta_key? Atom.to_string(key)
-  defp meta_key?(key) when is_binary(key), do: Regex.match? ~r/^__/, key
-  defp meta_key?(_),                       do: true
 
   defp copy_values(keys, source, target) do
     Enum.reduce(keys, target, fn key, target -> copy_value(key, source, target) end)
