c64a78d986c38f6bd4a3753a49a01bf2ce39a6a4
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-23 20:23:58 -0500

    Removed duplication in struct and map transformers

--- lib/transform/map.ex
+++ lib/transform/map.ex
@@ -1,24 +1,27 @@
 defmodule Transform.Map do
-  def transform(source, target) when is_map(target) do
+  def transform(source, target, fun \\ &target_key/1) when is_map(target) do
     source
     |> Map.keys
     |> reject_meta_keys
-    |> copy_values(source, target)
+    |> copy_values(source, target, fun)
   end
 
-  def reject_meta_keys keys do
+  defp reject_meta_keys keys do
     Enum.reject keys, &meta_key?(&1)
   end
 
   defp meta_key?(key) when is_atom(key),   do: meta_key? Atom.to_string(key)
   defp meta_key?(key) when is_binary(key), do: Regex.match? ~r/^__/, key
-  defp meta_key?(_),                       do: true
 
-  defp copy_values(keys, source, target) do
-    Enum.reduce(keys, target, &copy_value(&1, source, &2))
+  defp copy_values(keys, source, target, fun) do
+    Enum.reduce(keys, target, &copy_value(&1, source, &2, fun))
   end
 
-  defp copy_value(key, source, target) do
-    Map.put(target, key, Map.get(source, key))
+  defp copy_value(key, source, target, target_key) do
+    Map.put(target, target_key.(key), Map.get(source, key))
+  end
+
+  defp target_key(key) do
+    key
   end
 end
--- lib/transform/struct.ex
+++ lib/transform/struct.ex
@@ -4,20 +4,9 @@ defmodule Transform.Struct do
   end
 
   def transform(source, target = %{__struct__: _}) do
-    source
-    |> Map.keys
-    |> Transform.Map.reject_meta_keys
-    |> copy_values(source, target)
+    Transform.Map.transform source, target, &target_key/1
   end
 
-  defp copy_values(keys, source, target) do
-    Enum.reduce(keys, target, fn key, target -> copy_value(key, source, target) end)
-  end
-
-  defp copy_value(key, source, target) do
-    Map.put(target, to_atom(key), Map.get(source, key))
-  end
-
-  defp to_atom(key) when is_atom(key), do: key
-  defp to_atom(key),                   do: String.to_atom(key)
+  defp target_key(key) when is_atom(key),   do: key
+  defp target_key(key) when is_binary(key), do: String.to_atom(key)
 end
