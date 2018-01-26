5885d33f1d0457a4e9053fd821766a444305d81f
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-25 22:33:26 -0500

    Minor refactoring

--- test/transform/type_test.exs
+++ test/transform/type_test.exs
@@ -1,50 +1,50 @@
-defmodule Transform.TypeTest do
+defmodule TypeTest do
   use ExUnit.Case
   alias Transform.Type
 
   describe "primitives" do
     test ":integer" do
-      assert Transform.Type.transform("123", :integer) === {:ok, 123}
+      assert Type.transform("123", :integer) === {:ok, 123}
     end
 
     test ":string" do
-      assert Transform.Type.transform(123, :string) === {:ok, "123"}
+      assert Type.transform(123, :string) === {:ok, "123"}
     end
 
     test ":binary" do
-      assert Transform.Type.transform(123, :binary) === {:ok, "123"}
+      assert Type.transform(123, :binary) === {:ok, "123"}
     end
 
     test ":float" do
-      assert Transform.Type.transform("123", :float) === {:ok, 123.0}
+      assert Type.transform("123", :float) === {:ok, 123.0}
     end
 
     test ":decimal" do
-      assert Transform.Type.transform("123", :decimal) === {:ok, Decimal.new(123)}
+      assert Type.transform("123", :decimal) === {:ok, Decimal.new(123)}
     end
 
     test ":boolean" do
-      assert Transform.Type.transform("true", :boolean) === {:ok, true}
+      assert Type.transform("true", :boolean) === {:ok, true}
     end
 
     test ":date" do
-      assert Transform.Type.transform("1970-01-01", :date) === {:ok, ~D[1970-01-01]}
+      assert Type.transform("1970-01-01", :date) === {:ok, ~D[1970-01-01]}
     end
 
     test ":naive_datetime" do
-      assert Transform.Type.transform("1970-01-01 00:00:00", :naive_datetime) === {:ok, ~N[1970-01-01 00:00:00]}
+      assert Type.transform("1970-01-01 00:00:00", :naive_datetime) === {:ok, ~N[1970-01-01 00:00:00]}
     end
 
     test ":utc_datetime" do
-      assert Transform.Type.transform("1970-01-01 00:00:00", :utc_datetime) === DateTime.from_unix(0)
+      assert Type.transform("1970-01-01 00:00:00", :utc_datetime) === DateTime.from_unix(0)
     end
 
     test ":time" do
-      assert Transform.Type.transform("00:00:00", :time) === {:ok, ~T[00:00:00]}
+      assert Type.transform("00:00:00", :time) === {:ok, ~T[00:00:00]}
     end
 
     test "error is raised when primitive is invalid" do
-      assert_raise UndefinedFunctionError, fn -> Transform.Type.transform(123, :foo) end
+      assert_raise UndefinedFunctionError, fn -> Type.transform(123, :foo) end
     end
   end
 
