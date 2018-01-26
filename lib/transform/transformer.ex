d2137539584f0f5391ea4d2fe450569e6e4c120d
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-25 20:53:11 -0500

    Split and renamed files

---
+++ lib/transform/transformer.ex
@@ -0,0 +1,22 @@
+defmodule Transform.Transformer do
+  require Timex
+
+  def transform(map, mod) when is_map(map) and is_atom(mod) do
+    transform(map, mod.__transforms__)
+  end
+
+  def transform(map, transforms) when is_map(map) and is_list(transforms) do
+    Enum.reduce transforms, map, &transform(&2, &1)
+  end
+
+  def transform(map, {key, transforms}) when is_map(map) do
+    Enum.reduce(transforms, map, fn transform, input ->
+      {:ok, value} = transform(Map.get(input, key), transform)
+      Map.put(input, key, value)
+    end)
+  end
+
+  def transform(string, {:date, options}) when is_binary(string) do
+    Timex.Parse.DateTime.Parser.parse(string, options)
+  end
+end
--- test/transform/transform_test.exs
+++ test/transform/transform_test.exs
@@ -1,35 +1,12 @@
-defmodule Transform.Transformer do
-  require Timex
-
-  def transform(map, mod) when is_map(map) and is_atom(mod) do
-    transform(map, mod.__transforms__)
-  end
-
-  def transform(map, transforms) when is_map(map) and is_list(transforms) do
-    Enum.reduce transforms, map, &transform(&2, &1)
-  end
-
-  def transform(map, {key, transforms}) when is_map(map) do
-    Enum.reduce(transforms, map, fn transform, input ->
-      {:ok, value} = transform( Map.get(input, key), transform)
-      Map.put(input, key, value)
-    end)
-  end
-
-  def transform(string, {:date, options}) when is_binary(string) do
-    Timex.Parse.DateTime.Parser.parse(string, options)
-  end
-end
-
-defmodule Transform.TransformerTest do
+defmodule Transform.TransformTest do
   use ExUnit.Case
   import Transform.Transformer
 
-  defmodule Example do
+  defmodule Source do
     defstruct [:dob]
   end
 
-  defmodule DateConverter do
+  defmodule Example do
     use Transform.Transform
 
     transform do
@@ -39,7 +16,7 @@ defmodule Transform.TransformerTest do
 
   describe "transform" do
     test "date parsing" do
-      result = transform %Example{dob: "2001-01-01"}, DateConverter
+      result = transform %Source{dob: "2001-01-01"}, Example
       assert result.dob == ~N[2001-01-01 00:00:00]
     end
   end
