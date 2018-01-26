ce3b67dd20505abac59b43996c473f8c902b91ce
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-25 20:43:05 -0500

    Refactored Transformer

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,6 +2,6 @@ includes = []
 excludes = [:skip]
 
 # Uncomment the line below to enable focus mode
-includes = [:focus | includes]; excludes = [:test]
+# includes = [:focus | includes]; excludes = [:test]
 
 ExUnit.start(include: includes, exclude: excludes)
--- test/transform/transform_test.exs
+++ test/transform/transform_test.exs
@@ -1,26 +1,29 @@
 defmodule Transform.Transformer do
   require Timex
 
-  def transform(value, transforms) when is_list(transforms) do
-    Enum.reduce transforms, value, &transform2(&2, &1)
+  def transform(map, mod) when is_map(map) and is_atom(mod) do
+    transform(map, mod.__transforms__)
   end
 
-  defp transform2(map, {key, transforms}) when is_map(map) do
+  def transform(map, transforms) when is_map(map) and is_list(transforms) do
+    Enum.reduce transforms, map, &transform(&2, &1)
+  end
+
+  def transform(map, {key, transforms}) when is_map(map) do
     Enum.reduce(transforms, map, fn transform, input ->
-      original = Map.get(input, key)
-      {:ok, value} = transform3(original, transform)
+      {:ok, value} = transform( Map.get(input, key), transform)
       Map.put(input, key, value)
     end)
   end
 
-  def transform3(value, {:date, options}) do
-    Timex.Parse.DateTime.Parser.parse(value, options)
+  def transform(string, {:date, options}) when is_binary(string) do
+    Timex.Parse.DateTime.Parser.parse(string, options)
   end
 end
 
 defmodule Transform.TransformerTest do
   use ExUnit.Case
-  alias Transform.Transformer
+  import Transform.Transformer
 
   defmodule Example do
     defstruct [:dob]
@@ -35,10 +38,8 @@ defmodule Transform.TransformerTest do
   end
 
   describe "transform" do
-    @tag :focus
     test "date parsing" do
-      source = %Example{dob: "2001-01-01"}
-      result = Transformer.transform source, DateConverter.__transforms__()
+      result = transform %Example{dob: "2001-01-01"}, DateConverter
       assert result.dob == ~N[2001-01-01 00:00:00]
     end
   end
