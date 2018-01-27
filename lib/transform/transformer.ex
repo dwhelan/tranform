20ba76e2366183100ce345753ba55f1732e46694
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-26 21:11:15 -0500

    Added ability to format dates.

--- lib/transform/transformer.ex
+++ lib/transform/transformer.ex
@@ -1,20 +1,29 @@
 defmodule Transform.Transformer do
   
+  def transform(value, transform) when transform in [:string] do
+    Transform.Type.transform(value, transform)
+  end
+
   def transform(map, mod) when is_map(map) and is_atom(mod) do
     transform map, mod.__transforms__
   end
+  
   def transform(map, transforms) when is_map(map) and is_list(transforms) do
     Enum.reduce transforms, map, &transform(&2, &1)
   end
+  
   def transform(map, {key, transforms}) when is_map(map) and is_list(transforms) do
     transform(map, key, transforms)
   end
+  
   def transform(value, {transform, options}) do
     Transform.Type.transform(value, transform, options)
   end
+  
   def transform(value, transforms) when is_list(transforms) do
     Enum.reduce transforms, value, &transform(&2, &1)
   end
+  
   def transform(value, transform) when is_atom(transform) do
     Transform.Type.transform(value, transform)
   end
--- test/transform/transformer_test.exs
+++ test/transform/transformer_test.exs
@@ -3,9 +3,14 @@ defmodule Transform.TransformerTest do
   import Transform.Transformer
 
   describe "transform" do
-    test ":date" do
+    test "string => :date" do
       {:ok, result} = transform "2001-01-01", :date
       assert result == ~D[2001-01-01]
+    end
+
+    test ":data => string" do
+      {:ok, result} = transform ~D[2001-01-01], :string
+      assert result ==  "2001-01-01"
     end
 
     test ":date with parse options" do
