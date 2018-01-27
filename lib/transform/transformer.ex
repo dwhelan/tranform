f65c20c32b1371c535b9e96a27dc174fe6081828
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-26 21:15:25 -0500

    Refactored Transformer

--- lib/transform/transformer.ex
+++ lib/transform/transformer.ex
@@ -1,15 +1,15 @@
 defmodule Transform.Transformer do
   
-  def transform(value, transform) when transform in [:string] do
+  def transform(value, transform) when transform in [:string, :date] do
     Transform.Type.transform(value, transform)
   end
 
-  def transform(map, mod) when is_map(map) and is_atom(mod) do
-    transform map, mod.__transforms__
+  def transform(value, mod) when is_atom(mod) do
+    transform value, mod.__transforms__
   end
   
-  def transform(map, transforms) when is_map(map) and is_list(transforms) do
-    Enum.reduce transforms, map, &transform(&2, &1)
+  def transform(value, transforms) when is_list(transforms) do
+    Enum.reduce transforms, value, &transform(&2, &1)
   end
   
   def transform(map, {key, transforms}) when is_map(map) and is_list(transforms) do
