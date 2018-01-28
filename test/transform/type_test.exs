f4440d58ff3f9cadb7ee85733e21afe0320f6694
Declan Whelan <dwhelan@dpwhelan.com>
2018-01-28 01:47:09 -0500

    Added tests for converting from Time structs

--- test/transform/type_test.exs
+++ test/transform/type_test.exs
@@ -203,7 +203,6 @@ defmodule TypeTest do
     end
   end
 
-
   describe "from NaiveDateTime to" do
     test ":date" do
       assert Type.transform(~N[1970-01-01 00:00:00], :date) === {:ok, ~D[1970-01-01]}
@@ -227,6 +226,28 @@ defmodule TypeTest do
 
     test ":binary" do
       assert Type.transform(~N[1970-01-01 00:00:00], :string) === {:ok, "1970-01-01 00:00:00"}
+    end
+  end
+
+  describe "from Time to" do
+    test ":date" do
+      assert Type.transform(~T[00:00:00], :date) === :error
+    end
+
+    test ":naive_datetime" do
+      assert Type.transform(~T[00:00:00], :naive_datetime) === :error
+    end
+
+    test ":time" do
+      assert Type.transform(~T[00:00:00], :time) === {:ok, ~T[00:00:00]}
+    end
+
+    test ":string" do
+      assert Type.transform(~T[00:00:00], :string) === {:ok, "00:00:00"}
+    end
+
+    test ":binary" do
+      assert Type.transform(~T[00:00:00], :binary) === {:ok, "00:00:00"}
     end
   end
 
